943235961e4aba8ab8cb5a91d2c76608b4d675c7
VishwasKrishnamurthy <vishwaskrishna070@gmail.com>
2026-01-09 00:25:38 +0530

    Add files via upload

---
+++ models/booking.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const bookingSchema = new mongoose.Schema({
+  listing: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Listing",
+    required: true,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  startDate: {
+    type: Date,
+    required: true,
+  },
+  endDate: {
+    type: Date,
+    required: true,
+  },
+  totalPrice: Number,
+  status: {
+    type: String,
+    enum: ["pending", "confirmed", "cancelled"],
+    default: "pending",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    expires: 600, // ⏱️ auto-delete pending bookings after 10 mins
+  },
+});
+
+module.exports = mongoose.model("Booking", bookingSchema);
